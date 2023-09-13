29d3fc737e1505ae999f02290b6836ae9fbb1966
ljonesdesign <larry@ljonesdesign.com>
2023-09-13 11:51:46 -0400

    move code to respositor

---
+++ code-samples/Chapter 02/02_02_double_quotes.py
@@ -0,0 +1,11 @@
+---
+title: Output.py
+sidebar_label: 02-02 double_quotes.py
+sidebar_position: 2
+---
+
+```python
+print("Kate Austen")
+print("123 Dharma Lane")
+print("Asheville, NC 28899")
+```
---
+++ code-samples/Chapter 05/05_01_function_demo.py
@@ -0,0 +1,10 @@
+def message(king_name, kingdom = 'Egypt'):
+ """Display devault value"""
+ print(f'{king_name} was a ruler over {kingdom}.')
+
+# Call the message function.
+message('Tut')
+message('Ramesses')
+message('Thutmose')
+message('Hatshepsut')
+message('Amenhotep')
